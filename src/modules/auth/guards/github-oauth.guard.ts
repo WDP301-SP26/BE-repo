d876e0b19b3c6ea0ed0d57b168fedb594ba34c3c
AbandonedDemon <hoanghiepgamer@gmail.com>
2026-01-29 00:21:36 +0700

    fix: eslint warnings and failed test cases

--- src/modules/auth/guards/github-oauth.guard.ts
+++ src/modules/auth/guards/github-oauth.guard.ts
@@ -1,11 +1,12 @@
 import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Injectable()
 export class GitHubOAuthGuard extends AuthGuard('github') {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const result = (await super.canActivate(context)) as boolean;
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<Request>();
     await super.logIn(request);
     return result;
   }
