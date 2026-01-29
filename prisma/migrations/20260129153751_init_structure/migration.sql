7d44dbb7d8cf5c55b91283524665601da281761c
ThanhTNV <thanhtran190304@gmail.com>
2026-01-29 22:45:31 +0700

    fix: remove old migrations and create new init migration

--- prisma/migrations/20260129153751_init_structure/migration.sql
+++ prisma/migrations/20260129153751_init_structure/migration.sql
@@ -1,14 +1,6 @@
-/*
-  Warnings:
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('STUDENT', 'GROUP_LEADER', 'LECTURER', 'ADMIN');
 
-  - The values [USER] on the enum `Role` will be removed. If these variants are still used in the database, this will fail.
-  - You are about to drop the `_CategoryToPost` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `categories` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `posts` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `profiles` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "AuthProvider" AS ENUM ('EMAIL', 'GITHUB', 'JIRA');
 
@@ -20,43 +12,6 @@ CREATE TYPE "GroupStatus" AS ENUM ('ACTIVE', 'ARCHIVED', 'COMPLETED');
 
 -- CreateEnum
 CREATE TYPE "MembershipRole" AS ENUM ('MEMBER', 'LEADER', 'MENTOR');
-
--- AlterEnum
-BEGIN;
-CREATE TYPE "Role_new" AS ENUM ('STUDENT', 'GROUP_LEADER', 'LECTURER', 'ADMIN');
-ALTER TABLE "users" ALTER COLUMN "role" DROP DEFAULT;
-ALTER TABLE "User" ALTER COLUMN "role" TYPE "Role_new" USING ("role"::text::"Role_new");
-ALTER TYPE "Role" RENAME TO "Role_old";
-ALTER TYPE "Role_new" RENAME TO "Role";
-DROP TYPE "Role_old";
-COMMIT;
-
--- DropForeignKey
-ALTER TABLE "_CategoryToPost" DROP CONSTRAINT "_CategoryToPost_A_fkey";
-
--- DropForeignKey
-ALTER TABLE "_CategoryToPost" DROP CONSTRAINT "_CategoryToPost_B_fkey";
-
--- DropForeignKey
-ALTER TABLE "posts" DROP CONSTRAINT "posts_authorId_fkey";
-
--- DropForeignKey
-ALTER TABLE "profiles" DROP CONSTRAINT "profiles_userId_fkey";
-
--- DropTable
-DROP TABLE "_CategoryToPost";
-
--- DropTable
-DROP TABLE "categories";
-
--- DropTable
-DROP TABLE "posts";
-
--- DropTable
-DROP TABLE "profiles";
-
--- DropTable
-DROP TABLE "users";
 
 -- CreateTable
 CREATE TABLE "User" (
