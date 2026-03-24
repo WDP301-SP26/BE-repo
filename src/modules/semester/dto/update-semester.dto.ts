3da49149b9883074033d49005f9ed5ab215658de
baole405 <baolttse184569@fpt.edu.vn>
2026-03-24 19:00:28 +0700

    fix: type

--- src/modules/semester/dto/update-semester.dto.ts
+++ src/modules/semester/dto/update-semester.dto.ts
@@ -2,4 +2,3 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateSemesterDto } from './create-semester.dto';
 
 export class UpdateSemesterDto extends PartialType(CreateSemesterDto) {}
-
